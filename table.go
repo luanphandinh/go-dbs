1f601a2c98eb91b038cf132af8b01d22226f986a
Luan Phan <luanphandinh@outlook.com>
2019-08-31 21:02:23 +0700

    Split table validations

--- table.go
+++ table.go
@@ -7,15 +7,31 @@ type Table struct {
 	Columns []Column `json:"columns"`
 }
 
-func (table *Table) Validate() error  {
+func (table *Table) ValidateName() error {
 	if table.Name == "" {
 		return fmt.Errorf("table name should not empty")
 	}
 
+	return nil
+}
+
+func (table *Table) ValidateColumns() error  {
 	for _, col := range table.Columns {
 		if err := col.Validate(); err != nil {
 			return err
 		}
+	}
+
+	return nil
+}
+
+func (table *Table) Validate() error  {
+	if err := table.ValidateName(); err != nil {
+		return err
+	}
+
+	if err := table.ValidateColumns(); err != nil {
+		return err
 	}
 
 	return nil
