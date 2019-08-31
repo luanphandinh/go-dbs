3a525023090cd4e125337e2e1af3faeaf9282722
Luan Phan <luanphandinh@outlook.com>
2019-08-31 10:02:38 +0700

    Schema column

--- schema.go
+++ schema.go
@@ -1,5 +1,10 @@
 package go_db
 
+import (
+	"fmt"
+	"log"
+)
+
 type Schema struct {
 	Name   string  `json:"name"`
 	Tables []Table `json:"tables"`
@@ -16,4 +21,39 @@ type Column struct {
 	NotNull       bool   `json:"not_null"`
 	Primary       bool   `json:"primary"`
 	AutoIncrement bool   `json:"auto_increment"`
+}
+
+
+func (col *Column) validate() error {
+	if col.Name == "" {
+		return fmt.Errorf("column name should not empty")
+	}
+
+	if col.Type == "" {
+		return fmt.Errorf("column type should not empty")
+	}
+
+	if col.Type != "INT" && col.AutoIncrement {
+		log.Fatal(fmt.Sprintf("%s can not auto_increment", col.Type))
+	}
+
+	return nil
+}
+
+func (col *Column) toString() string {
+	columnString := fmt.Sprintf("%s %s", col.Name, col.Type)
+
+	if col.AutoIncrement {
+		columnString += " AUTO_INCREMENT"
+	}
+
+	if col.Primary {
+		columnString += " PRIMARY KEY"
+	}
+
+	if col.NotNull {
+		columnString += " NOT NULL"
+	}
+
+	return columnString
 }
