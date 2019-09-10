5a361f171f87ac959a71bd635e51bab9735dbc72
Luan Phan <luanphandinh@outlook.com>
2019-09-10 09:05:21 +0700

    update README

--- db_postgresql_platform.go
+++ db_postgresql_platform.go
@@ -21,10 +21,6 @@ func (platform *PostgresPlatform) GetDBConnectionString(server string, port int,
 	)
 }
 
-func (platform *PostgresPlatform) GetSchemaDeclarationSQL(schema string) string {
-	return schema
-}
-
 func (platform *PostgresPlatform) GetTypeDeclaration(col *Column) string {
 	return col.Type
 }
