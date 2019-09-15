3fb81a2488e83b8199208f5f5be2a0af6769dc6b
Luan Phan <luanphandinh@outlook.com>
2019-09-15 10:42:47 +0700

    SQlite Platform > stop using fmt.Sprintf(..)

--- db_sqlite_platform.go
+++ db_sqlite_platform.go
@@ -1,11 +1,10 @@
 package dbs
 
-import "fmt"
+import "strconv"
 
 const SQLITE3 string = "sqlite3"
 
-type SqlitePlatform struct {
-}
+type SqlitePlatform struct{}
 
 func (platform *SqlitePlatform) GetDriverName() string {
 	return SQLITE3
@@ -30,7 +29,7 @@ func (platform *SqlitePlatform) GetTypeDeclaration(col *Column) string {
 	}
 
 	if col.Length > 0 {
-		return fmt.Sprintf("%s(%d)", dbType, col.Length)
+		return dbType + "(" + strconv.Itoa(col.Length) + ")"
 	}
 
 	return dbType
