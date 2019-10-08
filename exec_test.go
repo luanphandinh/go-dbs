40273a76a09434132f02d344caf819c55e337347
Luan Phan <luanphandinh@outlook.com>
2019-10-08 23:22:43 +0700

    add install benchmark

---
+++ exec_test.go
@@ -0,0 +1,19 @@
+package dbs
+
+import (
+	"database/sql"
+	"testing"
+)
+
+func BenchmarkSequential(b *testing.B) {
+	db, _ := sql.Open(
+		_getPlatform(platform).getDriverName(),
+		_getPlatform(platform).getDBConnectionString(serverName, 3306, user, password, dbName),
+	)
+	SetPlatform(platform, db)
+
+	for i := 0; i < b.N; i++ {
+		drop(getSchema())
+		install(getSchema())
+	}
+}
